c6f22112f0e52595e70d5222f2fa2003e8630d54
jinsu291 <wlstn291@naver.com>
2023-01-11 19:43:14 +0900

    start

---
+++ src/test/java/com/ll/exam/jump/JumpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.jump;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
